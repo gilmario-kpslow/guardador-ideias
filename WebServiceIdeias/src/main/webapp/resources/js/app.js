653dfb1d73e375aee63a9730f054b6de541f135b
gilmario-kpslow <gilmariosoftware@gmail.com>
2015-04-17 22:29:19 -0300

    Iniciando Paginador

--- WebServiceIdeias/src/main/webapp/resources/js/app.js
+++ WebServiceIdeias/src/main/webapp/resources/js/app.js
@@ -7,3 +7,21 @@ $(function () {
     guardador = new Guardador();
     guardador.iniciar();
 });
+
+teste = (function () {
+    var tag = "#tagPag";
+    var colunas = ['Nome', 'Telefone', 'Email'];
+    var dados1 = [['Gilmario', '88200034', 'meial@email.com'], ['teste', '987938745', 'meils@email.com'], ['Mails', '93489845', 'email@email.com']];
+    var dados2 = [['Tiago', 'sdfsdfsdf', 'meial@email.com'], ['teste', 'sdfsdfsdf', 'meils@email.com'], ['Mails', '93489845', 'email@email.com']];
+    var dados3 = [['gilvan', '88200034', 'sdfsdfsdfsdf'], ['teste', '987938745', 'meils@email.com'], ['Mails', '93489845', 'email@email.com']];
+    var dados4 = [['gilvan', '88200034', 'sdfsdfsdfsdf'], ['teste', '987938745', 'meils@email.com'], ['Mails', '93489845', 'email@email.com']];
+    var dados5 = [['gilvan', '88200034', 'sdfsdfsdfsdf'], ['teste', '987938745', 'meils@email.com'], ['Mails', '93489845', 'email@email.com']];
+    var dados6 = [['gilvan', '88200034', 'sdfsdfsdfsdf'], ['teste', '987938745', 'meils@email.com'], ['Mails', '93489845', 'email@email.com']];
+    var dados7 = [['gilvan', '88200034', 'sdfsdfsdfsdf'], ['teste', '987938745', 'meils@email.com'], ['Mails', '93489845', 'email@email.com']];
+    var dados8 = [['gilvan', '88200034', 'sdfsdfsdfsdf'], ['teste', '987938745', 'meils@email.com'], ['Mails', '93489845', 'email@email.com']];
+    var dados = [dados1, dados2, dados3, dados4, dados5, dados6, dados7, dados8];
+    paginador = new PaginadorMagico(tag, colunas, "#tabelaa", function (p) {
+        return dados[p];
+    }, dados.length);
+    paginador.iniciar();
+});
--- WebServiceIdeias/src/main/webapp/resources/js/guardador/util/PaginadorMagico.js
+++ WebServiceIdeias/src/main/webapp/resources/js/guardador/util/PaginadorMagico.js
@@ -1,32 +1,70 @@
-PaginadorMagico = (function (tag, colunas, campos) {
-    var tagPaginador = tag;
+PaginadorMagico = (function (tagPaginador, colunas, idTabela, consultar, numeroPaginas) {
+    var tagPaginador = tagPaginador;
+    var colunas = colunas;
+    var tabela = $(idTabela);
+    var consultar = consultar;
+    var paginaAtual = 1;
+    var numeroPaginas = numeroPaginas;
 
-    this.criarPaginador = (function (qtd, paginaAtual) {
-        var paginador = $(tag);
+    this.iniciar = (function () {
+        this.criarPaginador();
+        this.adicionaCabecalho(tabela);
+        this.paginar(paginaAtual);
+    });
+
+    this.criarPaginador = (function () {
+        var paginador = $(tagPaginador);
         paginador.html('');
         paginador.append("<li onclick='paginador.anterior();'><span>&laquo;</span></li>");
-        for (var i in qtd) {
+        for (var i = 1; i <= numeroPaginas; i++) {
             if (i === paginaAtual) {
-                paginador.append("<li class='active' page-index='" + i + "' onclick='paginador.paginar(this," + i + ");'><span>" + i + "</span></li>");
+                paginador.append("<li class='active' onclick='paginador.paginar(" + i + ");'><span>" + i + "</span></li>");
             } else {
-                paginador.append("<li page-index='" + i + "' onclick='paginador.paginar(this," + i + ");'><span>" + i + "</span></li>");
+                paginador.append("<li onclick='paginador.paginar(" + i + ");'><span>" + i + "</span></li>");
             }
         }
-
-
+        paginador.append("<li onclick='paginador.proximo();'><span>&raquo;</span></li>");
     });
-    this.montarTabela = (function () {
 
+    this.montarTabela = (function (linhas) {
+        tabela.children("tbody").remove();
+        var conteudo = "<tbody>";
+        for (var i = 0; i < linhas.length; i++) {
+            conteudo += "<tr>";
+            for (var j = 0; j < linhas[i].length; j++) {
+                conteudo += "<td>" + linhas[i][j] + "</td>";
+            }
+            conteudo += "</tr>";
+        }
+        conteudo += "</tbody>";
+        tabela.append(conteudo);
+    });
+
+    this.adicionaCabecalho = (function (tabela) {
+        tabela.html("");
+        var colunasUnidas = "<thead><tr>";
+        for (var i = 0; i < colunas.length; i++) {
+            colunasUnidas += "<th>" + colunas[i] + "</th>";
+        }
+        colunasUnidas += "</tr></thead>";
+        tabela.append(colunasUnidas);
     });
 
     this.proximo = (function () {
-
+        if (paginaAtual < numeroPaginas) {
+            this.paginar(paginaAtual + 1);
+        }
     });
-    this.voltar = (function () {
 
+    this.anterior = (function () {
+        if (paginaAtual > 1) {
+            this.paginar(paginaAtual - 1);
+        }
     });
-    this.paginar = (function () {
-
+    this.paginar = (function (pagina) {
+        paginaAtual = pagina;
+        this.criarPaginador();
+        this.montarTabela(consultar(pagina - 1));
     });
 
 
